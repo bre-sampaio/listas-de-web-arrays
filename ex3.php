6c3050734bfe669bcbb08436585262450ded7a05
Fran <Fran@DESKTOP-7N5H6TT>
2019-03-04 15:43:11 -0300

    Terminado

--- ex3.php
+++ ex3.php
@@ -11,19 +11,27 @@
   
             for($i=0; $i < 30; $i++){
                 $vetor[$i] = $i+1;
+                $soma +=  $vetor[$i];
             }
             
+            $resul = array();    
+            
+             for ($i=1; $i < 30; $i++){
+                 
+                 if($vetor[$i] > $vetor[$i - 1]){
+                     $resul[0] = $vetor[$i];
+                     $resul [1] = $i;
+                 }
+             }
             var_dump($vetor);
             
-            //questao a:
-                 echo "o maior elemento do vetor é: " .max($vetor);
-                
-            //questao b: 
-                
-                 
-            //questao c: 
-                
-            
+               echo "Maior valor: " .$resul[0];
+              
+               echo "<br> Pocição: " .$resul[1];
+               
+               echo "<br> Soma: " .$soma;
+               
+               echo "<br> Média: " .($soma / count ($vetor));
         ?>
     </body>
 </html>
